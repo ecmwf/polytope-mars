f8f1ab46d8d41a126917e24c5a497be77eaa76d1
awarde96 <awarde@tcd.ie>
2024-08-16 08:12:57 +0000

    Refactor config so now options are imported from polytope

--- polytope_mars/config.py
+++ polytope_mars/config.py
@@ -1,7 +1,5 @@
-from typing import Dict, List, Literal, Optional, Union
-
 from conflator import ConfigModel
-from pydantic import ConfigDict
+from polytope.options import Config
 
 
 class DatacubeConfig(ConfigModel):
@@ -9,65 +7,6 @@ class DatacubeConfig(ConfigModel):
     type: str = "gribjump"
     config: str = "config.yaml"
     uri: str = "http://localhost:8000"
-
-
-class TransformationConfig(ConfigModel):
-    model_config = ConfigDict(extra="forbid")
-    name: str = ""
-
-
-class CyclicConfig(TransformationConfig):
-    name: Literal["cyclic"]
-    range: List[float] = [0]
-
-
-class MapperConfig(TransformationConfig):
-    name: Literal["mapper"]
-    type: str = ""
-    resolution: Union[int, List[int]] = 0
-    axes: List[str] = [""]
-    local: Optional[List[float]] = None
-
-
-class ReverseConfig(TransformationConfig):
-    name: Literal["reverse"]
-    is_reverse: bool = False
-
-
-class TypeChangeConfig(TransformationConfig):
-    name: Literal["type_change"]
-    type: str = "int"
-
-
-class MergeConfig(TransformationConfig):
-    name: Literal["merge"]
-    other_axis: str = ""
-    linkers: List[str] = [""]
-
-
-action_subclasses_union = Union[
-    CyclicConfig, MapperConfig, ReverseConfig, TypeChangeConfig, MergeConfig
-]
-
-
-class AxisConfig(ConfigModel):
-    axis_name: str = ""
-    transformations: list[action_subclasses_union]
-
-
-path_subclasses_union = Union[str, int, float]
-
-
-class GribJumpAxesConfig(ConfigModel):
-    axis_name: str = ""
-    values: List[str] = [""]
-
-
-class Config(ConfigModel):
-    axis_config: List[AxisConfig] = []
-    compressed_axes_config: List[str] = [""]
-    pre_path: Optional[Dict[str, path_subclasses_union]] = {}
-    alternative_axes: List[GribJumpAxesConfig] = []
 
 
 class CovjsonKitConfig(ConfigModel):
