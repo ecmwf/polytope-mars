e6e1eca61ee6f5b8049e13ccfdc1ccc074db65bc
awarde96 <awarde@tcd.ie>
2025-01-10 14:59:00 +0000

    Update timeseries so axes can take a list of values, update tests

--- tests/test_polygon.py
+++ tests/test_polygon.py
@@ -26,13 +26,12 @@ class TestFeatureFactory:
             "levtype": "sfc",
             "expver": "0001",
             "domain": "g",
-            "param": "164/166/167/169",
-            "number": "1/to/2",
+            "param": "167/169",
+            "number": "1",
+            "step": "0",
             "feature": {
-                "type": "timeseries",
-                "points": [[-9.10, 38.78]],
-                "axes": "step",
-                "range": {"start": 0, "end": 3},
+                "type": "polygon",
+                "shape": [[40.0, -105.0], [40.0, -104.0], [41.0, -104.0], [41.0, -105.0], [40.0, -105.0]],
             },
         }
 
@@ -53,16 +52,17 @@ class TestFeatureFactory:
                         }
                     ],
                 },
-                {"axis_name": "levelist", "transformations": [{"name": "type_change", "type": "str"}]},
                 {"axis_name": "latitude", "transformations": [{"name": "reverse", "is_reverse": True}]},
                 {"axis_name": "longitude", "transformations": [{"name": "cyclic", "range": [0, 360]}]},
                 {"axis_name": "step", "transformations": [{"name": "type_change", "type": "int"}]},
                 {"axis_name": "number", "transformations": [{"name": "type_change", "type": "int"}]},
+                {"axis_name": "levelist", "transformations": [{"name": "type_change", "type": "int"}]},
             ],
             "compressed_axes_config": [
                 "longitude",
                 "latitude",
                 "levtype",
+                "levelist",
                 "step",
                 "date",
                 "domain",
@@ -80,6 +80,8 @@ class TestFeatureFactory:
                 "stream": "enfo",
                 "type": "pf",
                 "domain": "g",
+                "date": self.date,
+                "time": "0000",
             },
         }
 
@@ -88,36 +90,43 @@ class TestFeatureFactory:
         self.cf["options"] = self.options
 
     # @pytest.mark.skip(reason="Gribjump not set up for ci actions yet")
-    def test_timeseries(self):
+    def test_polygon(self):
         result = PolytopeMars(self.cf).extract(self.request)
         decoder = Covjsonkit().decode(result)
         decoder.to_xarray()
         assert True
 
-    def test_timeseries_axes(self):
-        self.request["feature"]["axes"] = ["latitude", "longitude", "step"]
+    # @pytest.mark.skip(reason="Gribjump not set up for ci actions yet")
+    def test_polygon_lonlat(self):
+        self.request["feature"]["axes"] = ["longitude", "latitude"]
+        self.request["feature"]["shape"] = [
+            [-105.0, 40.0],
+            [-104.0, 40.0],
+            [-104.0, 41.0],
+            [-105.0, 41.0],
+            [-105.0, 40.0],
+        ]
         result = PolytopeMars(self.cf).extract(self.request)
         decoder = Covjsonkit().decode(result)
         decoder.to_xarray()
         assert True
 
-    def test_timeseries_mix_axes(self):
-        self.request["feature"]["axes"] = ["longitude", "latitude", "step"]
-        result = PolytopeMars(self.cf).extract(self.request)
-        decoder = Covjsonkit().decode(result)
-        decoder.to_xarray()
-        assert True
-
-    def test_timeseries_step_in_both(self):
-        self.request["step"] = "0/to/3"
+    # @pytest.mark.skip(reason="Gribjump not set up for ci actions yet")
+    def test_polygon_1_axes(self):
+        self.request["feature"]["axes"] = ["latitude"]
+        self.request["feature"]["shape"] = [[-1], [0], [-1]]
         with pytest.raises(ValueError):
-            result = PolytopeMars(self.cf).extract(self.request)
-            decoder = Covjsonkit().decode(result)
-            decoder.to_xarray()
+            PolytopeMars(self.cf).extract(self.request)
 
-    def test_timeseries_step_in_request(self):
-        self.request["step"] = "0/to/3"
-        del self.request["feature"]["range"]
+    def test_polygon_multiple_steps(self):
+        self.request["step"] = "0/1"
+        result = PolytopeMars(self.cf).extract(self.request)
+        decoder = Covjsonkit().decode(result)
+        decoder.to_xarray()
+        assert True
+
+    def test_polygon_multiple_number(self):
+        self.request["number"] = "1/2"
         result = PolytopeMars(self.cf).extract(self.request)
         decoder = Covjsonkit().decode(result)
         decoder.to_xarray()
